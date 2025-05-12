7727b3a3c43becf242ad898548d82839feae3fa0
Shafat <shafat.jamil@orbitax.com>
2025-05-12 20:03:07 +0600

    linate info os almost there

--- cmd/info.go
+++ cmd/info.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	// "math"
 	"runtime"
-
+	// "strconv"
 	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
+	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/spf13/cobra"
 )
 
@@ -33,20 +37,53 @@ type OsInfo struct {
 	Architecture  string
 	Distribution  string
 	Version       string
-	TotalMemory   string
 	KernelVersion string
+	CPUCount      int
+	CPUModel      string
+	TotalMemory   uint64
+	TotalDisk     uint64
 }
+
+// type InfoStat struct {
+// 	CPU        int32    `json:"cpu"`
+// 	VendorID   string   `json:"vendorId"`
+// 	Family     string   `json:"family"`
+// 	Model      string   `json:"model"`
+// 	Stepping   int32    `json:"stepping"`
+// 	PhysicalID string   `json:"physicalId"`
+// 	CoreID     string   `json:"coreId"`
+// 	Cores      int32    `json:"cores"`
+// 	ModelName  string   `json:"modelName"`
+// 	Mhz        float64  `json:"mhz"`
+// 	CacheSize  int32    `json:"cacheSize"`
+// 	Flags      []string `json:"flags"`
+// 	Microcode  string   `json:"microcode"`
+// }
 
 type platform struct{}
 
 func os_info(cmd *cobra.Command, args []string) {
 	var osinfo OsInfo
+	
+	osinfo.Architecture = runtime.GOARCH
 	osinfo.Distribution, _, osinfo.Version, _ = host.PlatformInformation()
 	osinfo.KernelVersion, _ = host.KernelVersion()
-	osinfo.Architecture = runtime.GOARCH
 
+	cpuinfo, _ := cpu.Info()
+	osinfo.CPUCount, _ = cpu.Counts(true)
+	osinfo.CPUModel = cpuinfo[0].ModelName
+    
+	buff, _ := mem.VirtualMemory()
+	osinfo.TotalMemory = buff.Total / 1048576
+
+	diskinfo, _ := disk.Usage("/home/shafat-jamil")
+	osinfo.TotalDisk = diskinfo.Total / 1048576
+	
 	fmt.Print("OS: ", osinfo.Distribution, " ", osinfo.Version, "\n")
 	fmt.Print("Architecture: ", osinfo.Architecture, "\n")
 	fmt.Print("Kernel: ", osinfo.KernelVersion, "\n")
-
+	fmt.Print("CPU(s): ", osinfo.CPUCount, "\n")
+	fmt.Print("CPU Model: ", osinfo.CPUModel, "\n")
+	fmt.Print("Total Memory: ", osinfo.TotalMemory, " MB\n")
+    fmt.Print("Disk info: ", osinfo.TotalDisk, " MB\n")
 }
